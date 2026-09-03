5d67c351741de7117f6baa865cc7a303ec617a74
lithiumselenate <hankhe22@utexas.edu>
2026-09-03 17:42:22 -0400

    Added requirements of style

--- test1.cpp
+++ test1.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
 #include<cstdio>
 using namespace std;
+// Programmer: Tianxing(Hank) He
+// Date: 2026-09-03
+// Purpose: Recalling of fundamental C++ rules and grammar
 int main(){ 
-    int a[10][10];
-    int x;
-    printf("Input start number: ");
-    scanf("%d",&x);
-    for(int i=0;i<10;i++)
-        for(int j=0;j<10;j++)
-            a[i][j] = x + i*j;
-    printf("Hello World!\n");
-    for (int i=0;i<10;i++){
-        for (int j=0;j<10;j++)
-            printf("%d ", a[i][j]);
-        printf("\n");
+  int a[10][10];
+  int x;
+  printf("Input start number: ");
+  scanf("%d",&x);
+  for(int i=0;i<10;i++)
+    for(int j=0;j<10;j++)
+      a[i][j] = x + i*j;
+  printf("Hello World!\n");
+  for (int i=0;i<10;i++){
+    for (int j=0;j<10;j++)
+      printf("%d ", a[i][j]);
+    printf("\n");
     }
-    string s;
-    cin>>s;
-    cout<<"String: "<<s<<endl;
-    return 0;
+  string s;
+  cin>>s;
+  cout<<"String: "<<s<<endl;
+  return 0;
 }
